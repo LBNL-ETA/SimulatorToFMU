41bef26e9326c423d902642efa3bbd88e857f139
Thierry Nouidui <tsnouidui@lbl.gov>
2017-05-02 17:07:23 -0700

    Minor typo

--- parser/utilities/Simulator.py
+++ parser/utilities/Simulator.py
@@ -1,4 +1,4 @@
-# Main Python function to be modified to integrate the main simulator.
+# Main Python function to be modified to interface with the Simulator.
 
 def exchange(configuration_file, time, input_values, 
             input_names, output_names, write_results):
@@ -6,13 +6,12 @@ def exchange(configuration_file, time, input_values,
     Return  a list of output values from the Python-based Simulator.
     The order of the output values must match the order of the output names.    
 
-    :param configuration_file (String): filename for the model configurations
-    :param time (Float): Current simulation time
+    :param configuration_file (String): Model configuration name
+    :param time (Float): Current Simulation time
     :param input_values (Floats): Input values 
     :param input_names (Strings): Input names (same length as input_values)
-
     :param output_names (Strings): Output names
-    :param write_results (Float): save results to a file (1.0 for saving, 0.0 else)
+    :param write_results (Float): Save results to a file (1 to store, 0 else)
         
     Example:
         >>> configuration_file = 'config.json'
@@ -32,8 +31,8 @@ def exchange(configuration_file, time, input_values,
     # This function currently returns the input values. 
     # This will need to be adapted so it return the output_values instead.
     # Assign the vector of output values with dummy values.
-	# If the list of output names has only one name, then only a scalar 
-	# must be returned.
+    # If the list of output names has only one name, then only a scalar 
+    # must be returned.
     if (len(output_names) > 1):
         output_values = [1.0] * len(output_names)
     else:
