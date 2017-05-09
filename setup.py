74e958387cad78cf0b59e3162fc5a7928c16de6a
Thierry Nouidui <tsnouidui@lbl.gov>
2017-05-09 08:54:18 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+# Python setup file.
+# See http://packages.python.org/an_example_pypi_project/setuptools.html
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="SimulatorToFMU",
+    version="0.1.0",
+    author="Thierry S. Nouidui",
+    author_email="TSNouidui@lbl.gov",
+    description=("Package for exporting a Simulator as a Functional Mock-up Unit"),
+    long_description=read('README.rst'),
+    license="3-clause BSD",
+    url="https://github.com/LBNL-ETA/SimulatorToFMU/",
+    install_requires=['lxml',
+                      'jinja2'],
+    packages=['bin', 'doc', 'fmus', 'parser'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Utilities"
+    ],
+)
