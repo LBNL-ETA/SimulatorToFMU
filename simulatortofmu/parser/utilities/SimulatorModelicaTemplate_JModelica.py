12e6c92ec917792941677a0a5565bd7f82272f5b
Thierry Nouidui <tsnouidui@lbl.gov>
2017-05-17 15:19:34 -0700

    Added template for jModelica

---
+++ simulatortofmu/parser/utilities/SimulatorModelicaTemplate_JModelica.py
@@ -0,0 +1,9 @@
+from pymodelica import compile_fmu
+
+fmu_name = compile_fmu("{{model_name}}", "{{model_name}}.mo", 
+                       compiler_log_level = 'd', version="{{fmi_version}}", target="{{fmi_api}}",
+                       compiler_options={'extra_lib_dirs':["{{sim_lib_path}}"]})
+
+
+
+
