8c350eea95ae6e9ad231b8255d77a47f3af587e9
Thierry Nouidui <tsnouidui@lbl.gov>
2017-05-19 17:40:47 -0700

    Removed debug flag

--- simulatortofmu/parser/utilities/SimulatorModelicaTemplate_JModelica.py
+++ simulatortofmu/parser/utilities/SimulatorModelicaTemplate_JModelica.py
@@ -1,7 +1,7 @@
 from pymodelica import compile_fmu
 
 fmu_name = compile_fmu("{{model_name}}", "{{model_name}}.mo", 
-                       compiler_log_level = 'd', version="{{fmi_version}}", target="{{fmi_api}}",
+                       version="{{fmi_version}}", target="{{fmi_api}}",
                        compiler_options={'extra_lib_dirs':["{{sim_lib_path}}"]})
 
 
