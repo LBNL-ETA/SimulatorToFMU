64b1f5da92b1d253fc2c23cb1cf54ad177f43e08
Thierry Nouidui <tsnouidui@lbl.gov>
2017-07-31 11:04:14 -0700

    Fixed version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="SimulatorToFMU",
-    version="1.0.0rc4",
+    version="1.0.0rc5",
     author="Thierry S. Nouidui",
     author_email="TSNouidui@lbl.gov",
     description=("Package for exporting a Simulator as a Functional Mock-up Unit"),
